4d7e573b1f9b7ec430b1c9ad279c3bfa00d9a70d
agatarauzer <agatarauzer@gmail.com>
2022-08-24 22:54:32 +0200

    add exception handling

---
+++ src/main/java/com/agatarauzer/myBooks/exception/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.agatarauzer.myBooks.exception;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ErrorResponse {
+	
+	private int status;
+	private String message;
+	private long timeStamp;
+	
+	public ErrorResponse(int status, String message, long timeStamp) {
+		this.status = status;
+		this.message = message;
+		this.timeStamp = timeStamp;
+	}
+}

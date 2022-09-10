2a5dca7d61348dbe3667c4bb55ef0dd514651e64
agatarauzer <agatarauzer@gmail.com>
2022-09-10 23:09:30 +0200

    add core configuration and GoogleBooks Api config

---
+++ src/main/java/com/agatarauzer/myBooks/config/GoogleBooksApiConfig.java
@@ -0,0 +1,18 @@
+package com.agatarauzer.myBooks.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+
+@Getter
+@Component
+public class GoogleBooksApiConfig {
+	
+	@Value("${googleBooks.api.endpoint}")
+	private String googleBooksApiEndpoint;
+	
+	@Value("${googleBooks.api.key}")
+	private String googleBooksAppKey;
+
+}

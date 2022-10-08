c7f2909513afca76ad54f718284ecc30205dd89f
agatarauzer <agatarauzer@gmail.com>
2022-10-08 22:03:38 +0200

    add new test case in GoogleBookMapperTest

--- src/test/java/com/agatarauzer/myBooks/mapper/GoogleBookMapperTest.java
+++ src/test/java/com/agatarauzer/myBooks/mapper/GoogleBookMapperTest.java
@@ -24,8 +24,8 @@ public class GoogleBookMapperTest {
 	@Autowired
 	private GoogleBookMapper googleBookMapper;
 	
-	private GoogleBookForUserDto googleBookForUserDto;
 	private GoogleBooksSearchResultDto googleSearchResultDto; 
+	private GoogleBookForUserDto googleBookForUserDto;
 	
 	@BeforeEach
 	public void prepareTestData() {
@@ -47,14 +47,14 @@ public class GoogleBookMapperTest {
 	public void sholudMapGoogleBookForUserDtoToBook() {
 		Book book = googleBookMapper.mapToBook(googleBookForUserDto);
 		
-		assertEquals(book.getTitle(), googleBookForUserDto.getTitle());
-		assertEquals(book.getAuthors(), googleBookForUserDto.getAuthors());
-		assertEquals(book.getISBN(), googleBookForUserDto.getISBN());
-		assertEquals(book.getPublisher(), googleBookForUserDto.getPublisher());
-		assertEquals(book.getPublishingDate(), googleBookForUserDto.getPublishingDate());
-		assertEquals(book.getLanguage(), googleBookForUserDto.getLanguage());
-		assertEquals(book.getPages(), googleBookForUserDto.getPages());
-		assertEquals(book.getDescription(), googleBookForUserDto.getDescription());
+		assertEquals(googleBookForUserDto.getTitle(), book.getTitle());
+		assertEquals(googleBookForUserDto.getAuthors(), book.getAuthors());
+		assertEquals(googleBookForUserDto.getIsbn(), book.getIsbn());
+		assertEquals(googleBookForUserDto.getPublisher(), book.getPublisher());
+		assertEquals(googleBookForUserDto.getPublishingDate(), book.getPublishingDate());
+		assertEquals(googleBookForUserDto.getLanguage(), book.getLanguage());
+		assertEquals(googleBookForUserDto.getPages(), book.getPages());
+		assertEquals(googleBookForUserDto.getDescription(), book.getDescription());
 	}
 	
 	@Test
@@ -63,11 +63,31 @@ public class GoogleBookMapperTest {
 		
 		assertEquals("Java. Podstawy. Wydanie IX", booksForUser.get(0).getTitle());
 		assertEquals("Cay S. Horstmann, Gary Cornell", booksForUser.get(0).getAuthors());
-		assertEquals("8324677615, 9788324677610", booksForUser.get(0).getISBN());
+		assertEquals("8324677615, 9788324677610", booksForUser.get(0).getIsbn());
 		assertEquals("Helion", booksForUser.get(0).getPublisher());
 		assertEquals("2013-12-09", booksForUser.get(0).getPublishingDate());
 		assertEquals("pl", booksForUser.get(0).getLanguage());
 		assertEquals(864, booksForUser.get(0).getPages());
 		assertEquals("Kolejne wydanie tej cenionej książki zostało zaktualizowane o wszystkie nowości...", booksForUser.get(0).getDescription());
+	}
+	
+	@Test
+	public void shouldMapGoogleSearchResultDtoToGoogleBookForUserDto_WithMissingFields() {
+		GoogleBookDto googleBookDto = new GoogleBookDto("Java. Podstawy.", null, null, null, null, null, null, 0, null, null);	
+		SearchInfo searchInfo = new SearchInfo("empty");	
+		GoogleBookBasicDto googleBookBasicDto = new GoogleBookBasicDto(googleBookDto, searchInfo);
+		List<GoogleBookBasicDto> books = List.of(googleBookBasicDto);
+		googleSearchResultDto = new GoogleBooksSearchResultDto(books, 1);
+		
+		List<GoogleBookForUserDto> googleBookWithMissingFields = googleBookMapper.mapToGoogleBookForUserDtoList(googleSearchResultDto);
+		
+		assertEquals("Java. Podstawy.", googleBookWithMissingFields.get(0).getTitle());
+		assertEquals(null, googleBookWithMissingFields.get(0).getAuthors());
+		assertEquals(null, googleBookWithMissingFields.get(0).getIsbn());
+		assertEquals(null, googleBookWithMissingFields.get(0).getPublisher());
+		assertEquals(null, googleBookWithMissingFields.get(0).getPublishingDate());
+		assertEquals(null, googleBookWithMissingFields.get(0).getLanguage());
+		assertEquals(0, googleBookWithMissingFields.get(0).getPages());
+		assertEquals("empty", googleBookWithMissingFields.get(0).getDescription());
 	}
 }
